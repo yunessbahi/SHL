b4d34f0e1d8732ea233d85976cfe7fd9cdab6a09
Youness Bahi <bahi.yns@gmail.com>
2025-10-15 00:31:45 +0100

    disable eslint and ts checks during vercel build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // ❌ Do not block production builds because of ESLint errors
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // ❌ Do not block builds if there are TypeScript errors
-        ignoreBuildErrors: true,
-    },
+  eslint: {
+    // ❌ Do not block production builds because of ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ❌ Do not block builds if there are TypeScript errors
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
