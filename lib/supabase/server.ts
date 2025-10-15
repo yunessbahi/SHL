49580c1a2e15938ca0e9c2e956e287418e7196af
Youness Bahi <bahi.yns@gmail.com>
2025-10-15 02:53:40 +0100

    fix auth

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(supabaseUrl!, supabaseKey!, {
