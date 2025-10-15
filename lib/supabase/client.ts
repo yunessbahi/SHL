6a769dcb59d12244c3dbaa3d793975ff7ee55132
Youness Bahi <bahi.yns@gmail.com>
2025-10-15 01:58:52 +0100

    fix auth

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const createClient = () =>
   createBrowserClient(supabaseUrl!, supabaseKey!);
--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
