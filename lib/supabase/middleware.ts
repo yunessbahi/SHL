6d0d943d063564a4180b40d1501a1092284f0f8d
Youness Bahi <bahi.yns@gmail.com>
2025-10-21 22:23:10 +0100

    freature - smartlink

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
